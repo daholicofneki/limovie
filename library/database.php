8e1d4aa0f2934476117fe3240fec6c6a627e2f5a
daholicofneki <neki.arismi@gmail.com>
2013-06-23 22:00:32 +0700

    remove space in database.php

--- library/database.php
+++ library/database.php
@@ -4,15 +4,14 @@
  */
 
 class Database {
-        
+
         private $host           = 'localhost';
         private $username       = 'root';
         private $password       = '';
         private $db             = 'wordpress';
         private $connection;
         private $arr            = array ();
-        
-        
+
         /**
          * Constructor
          *
@@ -27,26 +26,26 @@ class Database {
                 {
                         if ( ! $this->connection)
                         {
-                                throw new Exception ('Connection Error');
+                            throw new Exception ('Connection Error');
                         }
                         else
                         {
-                                try
+                            try
+                            {
+                                if ( ! @mysql_select_db($this->db, $this->connection))
                                 {
-                                        if ( ! @mysql_select_db($this->db, $this->connection))
-                                        {
-                                               throw new Exception ('Connection Error'); 
-                                        }
+                                    throw new Exception ('Connection Error'); 
                                 }
-                                catch (Exception $m)
-                                {
-                                        echo " Error : ". $m->getMessage ();
-                                }
+                            }
+                            catch (Exception $m)
+                            {
+                                    echo " Error : ". $m->getMessage ();
+                            }
                         }
                 }
                 catch ( Exception $m)
                 {
-                        echo " Error : ". $m->getMessage ();
+                    echo " Error : ". $m->getMessage ();
                 }
                 
         }
@@ -60,19 +59,17 @@ class Database {
          */
         public function query ( $SQL = FALSE )
         {
-                if ($SQL)
+            if ($SQL)
+            {
+                try
                 {
-                        try
-                        {
-                                return mysql_query ($SQL);
-                        }
-                        catch ( Exception $m)
-                        {
-                                //echo "<br />";
-                               // echo " File : ". $m->getFile()."<br />";
-                                echo " Error : ". $m->getMessage ();
-                        }
+                    return mysql_query ($SQL);
                 }
+                catch ( Exception $m)
+                {
+                    echo " Error : ". $m->getMessage ();
+                }
+            }
         }
         
         /**
@@ -84,28 +81,26 @@ class Database {
          */
         public function get ( $SQL = FALSE , $tipe = 'all')
         {
-                if ($SQL)
+            if ($SQL)
+            {
+                $SQL = $this->query ($SQL);
+                if ( $SQL AND  mysql_num_rows($SQL) > 0 )
                 {
-                        $SQL = $this->query ($SQL);
-                        if ( $SQL AND  mysql_num_rows($SQL) > 0 )
+                    if ($tipe === 'all')
+                    {
+                        while ($row = mysql_fetch_object ($SQL))
                         {
-                                if ($tipe === 'all')
-                                {
-                                        while ($row = mysql_fetch_object ($SQL))
-                                        {
-                                                $this->arr[] = $row;
-                                        }
-                                        return $this->arr;
-                                }
-                                else
-                                {
-                                        return mysql_fetch_object ($SQL);
-                                }
+                                $this->arr[] = $row;
                         }
-                       
+                        return $this->arr;
+                    }
+                    else
+                    {
+                        return mysql_fetch_object ($SQL);
+                    }
                 }
+            }
         }
-        
 }
 
 /*  End class Database  */
