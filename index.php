f2a04251b77eb1ba047acd8b5fd210d2c57f79cb
daholicofneki <neki.arismi@gmail.com>
2013-06-23 22:02:18 +0700

    EOD 20130623

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once ('library/config.php');
+
+$a = new Translate;
+$word = "
+For a company that is so technologically smart with their algorithms, the reason why the Translate API came so late, and is now becoming a paid API, is because Google licenses technology for their Translate product for other companies. 
+The link you showed has a quote saying about how Google did that for abuse reasons. 
+People abuse Google Search and Youtube every day in mass quantities.";
+echo $a->convert($word, 'en','in');
--- library/config.php
+++ library/config.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Database Class
+ * Configuration File
  */
 
 require_once ('database.php');
+require_once ('movie.php');
+require_once ('translate.php');
